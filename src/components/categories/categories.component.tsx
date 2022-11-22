e0c76f3f429ed1a1815421d547cd337b491c41cd
Roberto Oliveira <feliperoberto092@gmail.com>
2022-11-22 08:27:15 -0300

    refactor: add Styled Components to Categories Component

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 import CategoryItem from '../category-item/category-item.component'
 
 // Styles
-import './categories.styles.css'
+import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 // Utilities
 import env from '../../config/env.config'
@@ -30,15 +30,15 @@ const Categories = () => {
   }, [])
 
   return (
-    <div className="categories-container">
-      <div className="categories-content">
+    <CategoriesContainer>
+      <CategoriesContent>
         {categories.map((category) => (
           <div key={category.id}>
             <CategoryItem category={category} />
           </div>
         ))}
-      </div>
-    </div>
+      </CategoriesContent>
+    </CategoriesContainer>
   )
 }
 
