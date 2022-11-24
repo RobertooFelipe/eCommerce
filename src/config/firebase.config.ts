0a74b03bdb8365a2ddb1a3e8e3d6b0c7bcce01b9
Roberto Oliveira <roberto.oliveira@anchieta.br>
2022-11-24 08:59:45 -0300

    feat: add dotenv variables in firebase config

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAVhWH3fFcpWm8CJ7G5cHh5c3j3cYQPDpI',
-  authDomain: 'ecommerce-5d6f1.firebaseapp.com',
-  projectId: 'ecommerce-5d6f1',
-  storageBucket: 'ecommerce-5d6f1.appspot.com',
-  messagingSenderId: '267429714949',
-  appId: '1:267429714949:web:95b30addb648c482a6106c'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 export const app = initializeApp(firebaseConfig)
