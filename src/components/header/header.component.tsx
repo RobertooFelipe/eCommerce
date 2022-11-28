fd31e2a48cad2a22dcc411ab20b71c7322a1fe29
RobertooFelipe <feliperoberto092@gmail.com>
2022-11-28 13:28:43 -0300

    feat: add Explore Page and initial Categories Overview Component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -28,12 +28,16 @@ const Header = () => {
     navigate('/sign-up')
   }
 
+  const handleExploreClick = () => {
+    navigate('/explore')
+  }
+
   return (
     <HeadeContainer>
       <HeaderTitle>E-Commerce</HeaderTitle>
 
       <HeaderItems>
-        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem onClick={handleExploreClick}>Explorar</HeaderItem>
         {!isAuthenticated && (
           <>
             <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
