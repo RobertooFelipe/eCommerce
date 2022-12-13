0d1147249c766e8764ba11f463672cc470a14d00
RobertooFelipe <feliperoberto092@gmail.com>
2022-12-13 09:57:36 -0300

    feat: ensure prducts can be removed from cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -7,6 +7,7 @@ interface ICartContext {
   products: CartProduct[]
   toggleCart: () => void
   addProductToCart: (product: Product) => void
+  removeProductFromCart: (productId: string) => void
 }
 
 interface IChildrenProps {
@@ -17,7 +18,8 @@ export const CartContext = createContext<ICartContext>({
   isVisible: false,
   products: [],
   toggleCart: () => {},
-  addProductToCart: () => {}
+  addProductToCart: () => {},
+  removeProductFromCart: () => {}
 })
 
 const CartContextProvider: FunctionComponent<IChildrenProps> = ({
@@ -46,9 +48,21 @@ const CartContextProvider: FunctionComponent<IChildrenProps> = ({
     setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
   }
 
+  const removeProductFromCart = (productId: string) => {
+    setProducts((products) =>
+      products.filter((product) => product.id !== productId)
+    )
+  }
+
   return (
     <CartContext.Provider
-      value={{ isVisible, products, toggleCart, addProductToCart }}>
+      value={{
+        isVisible,
+        products,
+        toggleCart,
+        addProductToCart,
+        removeProductFromCart
+      }}>
       {children}
     </CartContext.Provider>
   )
