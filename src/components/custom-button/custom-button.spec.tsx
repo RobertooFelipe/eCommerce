882653bade91d99f9a82e6900acdadffc8d8189f
RobertooFelipe <feliperoberto092@gmail.com>
2023-01-05 09:03:16 -0300

    test: ensure custom button is rendered with correct children

---
+++ src/components/custom-button/custom-button.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import CustomButton from './custum-buttom.component'
+
+describe('Custom Button', () => {
+  it('should render with correct children', () => {
+    const { getByText } = render(<CustomButton>Lorem ipsum</CustomButton>)
+
+    getByText('Lorem ipsum')
+  })
+})
