cd0b44b181d87590a248c644d4f286fadff66f31
RobertooFelipe <feliperoberto092@gmail.com>
2023-01-05 09:38:46 -0300

    test: ensure custom input renders with error if hasError is true

---
+++ src/components/custom-input/custom-input.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react'
+import Colors from '../../theme/theme.colors'
+import CustomInput from './custom-input.component'
+
+describe('Custom Input', () => {
+  it('should render with erroe if hasError is true', () => {
+    const { getByPlaceholderText } = render(
+      <CustomInput placeholder="Lorem ipsum" hasError={true} />
+    )
+
+    const input = getByPlaceholderText('Lorem ipsum')
+
+    expect(input).toHaveStyle({ border: `2px solid ${Colors.error}` })
+  })
+})
