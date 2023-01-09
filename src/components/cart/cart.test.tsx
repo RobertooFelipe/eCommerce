7054f3ed0f64a1f958323f9f4a5e2fd684c59c34
RobertooFelipe <feliperoberto092@gmail.com>
2023-01-09 17:50:20 -0300

    test: ensure cart product quantity is decreased on decrease click

--- src/components/cart/cart.test.tsx
+++ src/components/cart/cart.test.tsx
@@ -62,4 +62,27 @@ describe('Cart', () => {
 
     getByText('3')
   })
+
+  it('should decrease product quantity on decrease click', () => {
+    const { getByLabelText, getByText } = renderWithRedux(<Cart />, {
+      preloadedState: {
+        cartReducer: {
+          products: [
+            {
+              id: '1',
+              imageUrl: 'image_url',
+              name: 'Boné',
+              price: 100,
+              quantity: 2
+            }
+          ]
+        }
+      } as any
+    })
+
+    const decreaseButton = getByLabelText(/decrease quantity of boné/i)
+    userEvent.click(decreaseButton)
+
+    getByText('1')
+  })
 })
