8c56521c84a265abcde577a33b41feda4098c5ce
RobertooFelipe <feliperoberto092@gmail.com>
2023-01-10 18:49:32 -0300

    test: ensure sign up shows error if email already existis

--- src/pages/sign-up/sign-up.test.tsx
+++ src/pages/sign-up/sign-up.test.tsx
@@ -1,4 +1,7 @@
 import userEvent from '@testing-library/user-event'
+import * as firebaseAuth from 'firebase/auth'
+import { AuthErrorCodes } from 'firebase/auth'
+
 import { renderWithRedux } from '../../helpers/test.helpers'
 import SignUpPage from './sign-up.page'
 
@@ -70,5 +73,36 @@ describe('Sign Up', () => {
     userEvent.click(submitButton)
 
     await findByText(/A senha deve ter no mínimo 6 caracteres/i)
+  })
+
+  it('should show error if email already exists', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+
+    const { getByText, findByText, getByPlaceholderText } = renderWithRedux(
+      <SignUpPage />,
+      {}
+    )
+
+    mockFirebaseAuth.createUserWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.EMAIL_EXISTS })
+    )
+
+    const nameInput = getByPlaceholderText(/digite seu nome/i)
+    const lastName = getByPlaceholderText(/digite seu sobrenome/i)
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+    const passwordInput = getByPlaceholderText(/digite sua senha/i)
+    const passwordConfirmationInput = getByPlaceholderText('Confirme sua senha')
+
+    userEvent.type(nameInput, 'Lorem')
+    userEvent.type(lastName, 'Ipsum')
+    userEvent.type(emailInput, 'lorem@ipsum.com')
+    userEvent.type(passwordInput, '12345678')
+    userEvent.type(passwordConfirmationInput, '12345678')
+
+    const submitButton = getByText('Criar conta', { selector: 'button' })
+
+    userEvent.click(submitButton)
+
+    await findByText(/Este e-mail já esta em uso/i)
   })
 })
