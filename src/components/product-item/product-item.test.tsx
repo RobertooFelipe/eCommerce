6de700ee316450c9400b328e335276f01918f7dc
RobertooFelipe <feliperoberto092@gmail.com>
2023-01-10 07:25:08 -0300

    test: ensure product item show correct product

---
+++ src/components/product-item/product-item.test.tsx
@@ -0,0 +1,20 @@
+import { renderWithRedux } from '../../helpers/test.helpers'
+import ProductItem from './product-item.component'
+
+describe('Product Item', () => {
+  it('should show correct product', () => {
+    const product = {
+      id: '1',
+      imageUrl: 'image_url',
+      name: 'Camisa',
+      price: 100,
+      quantity: 10
+    }
+
+    const { getByText } = renderWithRedux(<ProductItem product={product} />, {})
+
+    getByText(/camisa/i)
+    getByText(/R$100/i)
+    getByText(/adicionar ao carrinho/i)
+  })
+})
